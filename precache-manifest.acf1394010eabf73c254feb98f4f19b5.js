8e3f6b4ee5a18fc45d0f766aa919f371ede7d633
Raiyan <raiyan.sharif.1234@gmail.com>
2020-08-09 16:06:45 +0600

    Updates

---
+++ precache-manifest.acf1394010eabf73c254feb98f4f19b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70643a88d3021aa7a5ad64d0bd9a75bc",
+    "url": "/ReactAntApp/index.html"
+  },
+  {
+    "revision": "046992828e17733a7b83",
+    "url": "/ReactAntApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "746dfb2d8658d65d3c91",
+    "url": "/ReactAntApp/static/js/2.2fe3ca1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactAntApp/static/js/2.2fe3ca1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "046992828e17733a7b83",
+    "url": "/ReactAntApp/static/js/main.2b76c517.chunk.js"
+  },
+  {
+    "revision": "5e1b2f9c9985f4aeaa27",
+    "url": "/ReactAntApp/static/js/runtime-main.efa6be86.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactAntApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
